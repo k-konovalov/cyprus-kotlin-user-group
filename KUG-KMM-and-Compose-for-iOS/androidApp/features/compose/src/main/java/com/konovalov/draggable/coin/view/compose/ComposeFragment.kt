2f35ba777d61a9b7674c1b6ee343c96403ce5b1f
Konovalov Kirill <k.konovalov@indriver.com>
2023-06-12 16:50:35 +0300

    fix android build errors (manifest, ktor deps)

--- KUG-KMM-and-Compose-for-iOS/androidApp/features/compose/src/main/java/com/konovalov/draggable/coin/view/compose/ComposeFragment.kt
+++ KUG-KMM-and-Compose-for-iOS/androidApp/features/compose/src/main/java/com/konovalov/draggable/coin/view/compose/ComposeFragment.kt
@@ -1,6 +1,7 @@
 package com.konovalov.draggable.coin.view.compose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -11,30 +12,40 @@ import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import com.example.myapplication.network.RickAndMortyHttpClient
 import com.google.accompanist.themeadapter.appcompat.AppCompatTheme
 import com.konovalov.draggable.coin.view.core.ui.theme.DraggableScaredViewTheme
+import java.util.concurrent.Executors
 
 class ComposeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ) = ComposeView(requireContext()).apply {
-        setContent {
-            setViewCompositionStrategy(
-                ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
-            )
-            AppCompatTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                    Text(text = "Hello world.")
+    ) = ComposeView(requireContext())
+        .apply {
+            setContent {
+                setViewCompositionStrategy(
+                    ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
+                )
+                AppCompatTheme {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colors.background
+                    ) {
+                        Greeting("Android")
+                        Text(text = "Hello world.")
+                    }
                 }
             }
         }
-    }
+/*        .also {
+            Executors.newSingleThreadExecutor().execute {
+                RickAndMortyHttpClient().getCharacters().also {
+                    Log.e(javaClass.simpleName, it.toString())
+                }
+            }
+        }*/
 }
 
 
